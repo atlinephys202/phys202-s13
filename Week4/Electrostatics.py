74a1c956f96af335718638d9127544cf65e7a008
atlinephys202 <atline@calpoly.edu>
2013-04-25 14:03:09 -0700

    adding interpolation & mask notes 425

---
+++ Week4/Electrostatics.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def pointPotential(X,Y,q,posx,posy):
+	'''Expects q in coulombs, and points in meters.
+	Gives Vxy when given origin point (x,y) and location (posx, posy)'''
+	Vxy=(8.89e9)*q/((X-posx)**2.+(Y-posy)**2.)**(1/2)
+	return Vxy
+
+def dipolePotential(x,y,q,posx,angle):
+	"""Set Vxy with equation below to check total potential in the x axis at posx.
+	Expects q in coulombs and points in meters."""
+	k=8.9e9
+	Vxy = k*q/((X-posx*cos(angle))**2.+Y**2.)**(1/2) + k*q/((X+posx*cos(-1*angle))**2.+Y**2.)**(1/2)
+	return Vxy
