139b58aaa4335b289a5d54b12449fa49258502fc
Aaron <atline@calpoly.edu>
2013-05-24 11:22:42 -0700

    m

--- Week4/Electrostatics.py
+++ Week4/Electrostatics.py
@@ -6,9 +6,9 @@ def pointPotential(X,Y,q,posx,posy):
 	Vxy=(8.89e9)*q/((X-posx)**2.+(Y-posy)**2.)**(1/2)
 	return Vxy
 
-def dipolePotential(x,y,q,posx,angle):
+def dipolePotential(X,Y,q,posx,angle):
 	"""Set Vxy with equation below to check total potential in the x axis at posx.
 	Expects q in coulombs and points in meters."""
 	k=8.9e9
-	Vxy = k*q/((X-posx*cos(angle))**2.+Y**2.)**(1/2) + k*q/((X+posx*cos(-1*angle))**2.+Y**2.)**(1/2)
+	Vxy = k*q/((X-posx*np.cos(angle))**2.+Y**2.)**(1/2) + k*q/((X+posx*np.cos(-1*angle))**2.+Y**2.)**(1/2)
 	return Vxy
